786753edc0f15d9e4b2124f678bc74b65bbeb40a
MarjoryMel <marjorymel48@gmail.com>
2024-06-05 19:06:24 -0300

    solution to exercise 1

--- cypress/integration/CACT-TAT.spec.js
+++ cypress/integration/CACT-TAT.spec.js
@@ -1,9 +1,22 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    beforeEach(function (){
         cy.visit('./src/index.html')
+    })
+
+    it('verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it.only('preenche os campos obrigatórios e envia o formulário', function(){
+        cy.get('#firstName').type('Marjory')
+        cy.get('#lastName').type('Lemos')
+        cy.get('#email').type('exemplo@exemplo.com')
+        cy.get('#open-text-area').type('teste')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
     })
 })
   
