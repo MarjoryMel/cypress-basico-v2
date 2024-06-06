f79e22e3d764928a4259595d5008bb689b671a4c
MarjoryMel <marjorymel48@gmail.com>
2024-06-06 10:05:46 -0300

    extra exercise solution 4

--- cypress/integration/CACT-TAT.spec.js
+++ cypress/integration/CACT-TAT.spec.js
@@ -31,10 +31,21 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.error').should('be.visible')
     })
 
-    it.only('campo de telefone continua vazio quando preenchido com valor não-numérico', function(){
+    it('campo de telefone continua vazio quando preenchido com valor não-numérico', function(){
         cy.get('#phone')
         .type('abdefg')
         .should('have.value', '')
+    })
+
+    it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
+        cy.get('#firstName').type('Marjory')
+        cy.get('#lastName').type('Lemos')
+        cy.get('#email').type('exemplo@exemplo.com')
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('Test')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
     })
 })
   
