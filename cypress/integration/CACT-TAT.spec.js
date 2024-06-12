72890f26681b017849e7bb1e5cb89b918da523d5
MarjoryMel <marjorymel48@gmail.com>
2024-06-12 15:37:25 -0300

    exercise solution using invoke('val')

--- cypress/integration/CACT-TAT.spec.js
+++ cypress/integration/CACT-TAT.spec.js
@@ -195,7 +195,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .should('be.visible')
     })
 
-    it.only('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', function() {
         cy.get('.success')
           .should('not.be.visible')
           .invoke('show')
@@ -210,6 +210,14 @@ describe('Central de Atendimento ao Cliente TAT', function() {
           .and('contain', 'Valide os campos obrigatórios!')
           .invoke('hide')
           .should('not.be.visible')
+      })
+
+      it.only('preenche a area de texto usando o comando invoke', function(){
+        const longText = Cypress._.repeat('0123456789', 20)
+
+        cy.get('#open-text-area')
+            .invoke('val', longText)
+            .should('have.value', longText)
       })
 })
   
