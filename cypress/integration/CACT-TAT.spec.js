368b10f12ba06a19ba1828a2c28871299c38473f
MarjoryMel <marjorymel48@gmail.com>
2024-06-12 15:51:40 -0300

    'find the cat' challenge solution

--- cypress/integration/CACT-TAT.spec.js
+++ cypress/integration/CACT-TAT.spec.js
@@ -220,7 +220,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             .should('have.value', longText)
       })
 
-      it.only('faz uma requisição HTTP', function(){
+      it('faz uma requisição HTTP', function(){
         cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
             .should(function(res){
                 const { status, statusText, body} = res
@@ -228,6 +228,12 @@ describe('Central de Atendimento ao Cliente TAT', function() {
                 expect(statusText).to.equal('OK')
                 expect(body).to.include('CAC TAT')
             })
+      })
+
+      it.only('desafio - encontre o gato', function(){
+        cy.get('#cat')
+        .invoke('show')
+        .should('be.visible')
       })
 })
   
